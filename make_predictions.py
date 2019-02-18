1853200e3186641acbb23d287704b03553ee7e25
vishnu_000 <vishnu_000@bitbucket.org>
2019-02-18 23:12:24 +0530

    added test script

---
+++ make_predictions.py
@@ -0,0 +1,19 @@
+from keras.callbacks import ModelCheckpoint
+
+count = 0
+videoFile = "Tom and Jerry 3.mp4"
+cap = cv2.VideoCapture(videoFile)
+frameRate = cap.get(5) #frame rate
+x=1
+while(cap.isOpened()):
+    frameId = cap.get(1) #current frame number
+    ret, frame = cap.read()
+    if (ret != True):
+        break
+    if (frameId % math.floor(frameRate) == 0):
+        filename ="test%d.jpg" % count;count+=1
+        cv2.imwrite(filename, frame)
+cap.release()
+print ("Done!")
+
+filepath="weights.best.hdf5"
